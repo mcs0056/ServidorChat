9ff164ea0a17926b589b133ccaad2e89c734ca4b
mcs056 <mcs0056@alu.medac.es>
2025-12-15 09:12:53 +0100

    Initial commit

---
+++ src/Cliente.java
@@ -0,0 +1,45 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Cliente {
+    static void main() {
+        //Host del servidor
+        final String HOST = "localhost";
+        //Puerto del servidor
+        final int PUERTO = 5000;
+        DataInputStream in = null;
+        DataOutputStream out = null;
+
+        try{
+            Socket socket = new Socket(HOST, PUERTO);
+
+            in = new DataInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
+            Scanner s = new Scanner(System.in);
+
+            System.out.println("Introduce tu nombre: ");
+            String nombre = s.nextLine();
+            out.writeUTF(nombre);
+
+            boolean salir = false;
+
+            while(!salir){
+                System.out.println(nombre + ": ");
+                String msg = s.nextLine();
+                out.writeUTF(msg);
+
+                if (msg.equalsIgnoreCase("FIN")){
+                    salir = true;
+                }else{
+                    String respuesta = in.readUTF();
+                    System.out.println(respuesta);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Error en el cliente.");
+        }
+    }
+}
---
+++ src/Multihilo.java
@@ -0,0 +1,44 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class Multihilo implements Runnable{
+
+    private Socket socket;
+
+    public Multihilo(Socket socket){
+        this.socket = socket;
+    }
+
+    @Override
+    public void run(){
+        String nombre = "Desconocido";
+
+        DataInputStream in = null;
+        DataOutputStream out = null;
+
+        try{
+            in = new DataInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
+
+            nombre = in.readUTF(); //Lee el nombre del cliente
+            System.out.println("Cliente conectado: " + nombre + " desde " + socket.getInetAddress());
+
+            boolean salir = false;
+
+            while(!salir){
+                String msg = in.readUTF();
+                System.out.println(nombre + ": " + msg);
+
+                if(msg.equalsIgnoreCase("FIN")){
+                    salir = true;
+                }else{
+                    out.writeUTF("Servidor > " + msg);
+                }
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
