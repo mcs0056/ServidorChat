8ba617ca0c31fb3fb7b2be54d729530cf8909e60
mcs056 <mcs0056@alu.medac.es>
2025-12-15 12:52:18 +0100

    Class 'Multihilo' modified

--- src/Fase_3/Multihilo.java
+++ src/Fase_3/Multihilo.java
@@ -25,7 +25,7 @@ public class Multihilo implements Runnable{
             out = new DataOutputStream(socket.getOutputStream());
 
             nombre = in.readUTF(); //Lee el nombre del cliente
-            System.out.println("Fase_4.Cliente conectado: " + nombre);
+            System.out.println("Cliente conectado: " + nombre);
 
             boolean salir = false;
 
