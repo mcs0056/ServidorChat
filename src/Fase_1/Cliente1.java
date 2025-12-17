cf01ce4750b5aa808dd844b5a0b328abc9e2bdcd
mcs056 <mcs0056@alu.medac.es>
2025-12-17 08:21:58 +0100

    Add comentarios

--- src/Fase_1/Cliente1.java
+++ src/Fase_1/Cliente1.java
@@ -6,29 +6,41 @@ import java.io.IOException;
 import java.net.Socket;
 
 public class Cliente1 {
-    static void main() {
+    public static void main(String[] args) {
+        //Host del servidor
         final String HOST = "localhost";
+        //Puerto del servidor
         final int PUERTO = 5000;
 
+        //Declaración de las variables
         Socket socket = null;
         DataInputStream in = null;
         DataOutputStream out = null;
 
         try{
+            //Mensaje informativo por consola
             System.out.println("Intentando conectar con el servidor...");
+            //Creación del socket y se establece conexión con servidor
             socket = new Socket(HOST, PUERTO);
 
+            //Inicialización flujo de entrada para recibir datos del servidor
             in = new DataInputStream(socket.getInputStream());
+            //Inicialización flujo de salida para enviar datos del servidor
             out = new DataOutputStream(socket.getOutputStream());
 
+            //Envío de mensaje al servidor
             out.writeUTF("Hola, soy el cliente 1.");
 
+            //Espera de respuesta
             String msg = in.readUTF();
+            //Respuesta recibida
             System.out.println("Respuesta del servidor: " + msg);
 
+            //Cierre de socket y flujos junto a él
             socket.close();
 
         } catch (IOException e) {
+            //Recoge y muestra cualquier posible error
             System.out.println("Error: " + e.getMessage());
         }
     }
--- src/Fase_1/Servidor.java
+++ src/Fase_1/Servidor.java
@@ -9,44 +9,52 @@ import java.net.Socket;
 public class Servidor {
     static void main() {
 
+        //Declaración de las variables
         ServerSocket server = null;
         Socket socket = null;
         DataInputStream in = null;
         DataOutputStream out = null;
 
+        //Puerto del servidor
         final int PUERTO = 5000;
 
         try{
+            //Se crea el ServerSocket con el puerto indicado
             server = new ServerSocket(PUERTO);
             System.out.println("Servidor a la espera");
 
+            //Servidor permanece en ejecución indefinidamente
             while(true){
+                //Espera a que un cliente se conecte
                 socket = server.accept();
-                System.out.println("Fase_4.Cliente detectado. El servidor se va a dormir.");
+                System.out.println("Cliente detectado. El servidor se va a dormir.");
 
+                //Simula una tarea larga
                 try{
                     Thread.sleep(15000);//15 segundos de bloqueo
                 }catch(InterruptedException e){
                     e.printStackTrace();
                 }
 
-                System.out.println("Fase_4.Cliente atendido con éxito después de la siesta.");
+                System.out.println("Cliente atendido con éxito después de la siesta.");
+                //Inicialización de flujos de comunicación
                 in = new DataInputStream(socket.getInputStream());
                 out = new DataOutputStream(socket.getOutputStream());
 
-                //Lectura de mensaje
+                //Lectura de mensaje enviado por cliente
                 String msg = in.readUTF();
                 System.out.println("Mensaje recibido: " + msg);
 
-                //Envío de mensaje
+                //Envío de respuesta al cliente
                 out.writeUTF("Saludos desde el Servidor.");
 
-                //Cierre socket
+                //Cierre de socket y flujos junto a él
                 socket.close();
                 System.out.println("Conexión terminada.");
             }
 
         } catch (IOException e) {
+            //Control errores
             throw new RuntimeException(e);
         }
     }
