a2a8f50fd509e191f3f4ce3e29f02741be197b7a
mcs056 <mcs0056@alu.medac.es>
2025-12-17 08:40:19 +0100

    Add comentarios Fase_2

--- src/Fase_2/Cliente.java
+++ src/Fase_2/Cliente.java
@@ -14,29 +14,37 @@ public class Cliente {
         // Puerto del servidor
         final int PORT = 5000;
 
+        //Declaración de variables
         Scanner sc = null;
         Socket socket = null;
         DataInputStream in = null;
         DataOutputStream out = null;
 
         try {
+            //Crear y conectar socket al sevidor
             socket = new Socket(HOST, PORT);
 
+            //Entrada, salida y sc
             in = new DataInputStream(socket.getInputStream());
             out = new DataOutputStream(socket.getOutputStream());
             sc = new Scanner(System.in);
 
+            //Variable para salir
             boolean salir = false;
 
             while (!salir) {
+                //Solicitar escribir mensaje
                 System.out.print("Escribe un mensaje: ");
                 String mensaje = sc.nextLine();
 
+                //Envía mensaje al servidor
                 out.writeUTF(mensaje);
 
+                //Recibe respuesta del servidor
                 String respuesta = in.readUTF();
                 System.out.println(respuesta);
 
+                //Caso de que mensaje sea FIN, finaliza
                 if (mensaje.equalsIgnoreCase("FIN")) {
                     salir = true;
                 }
@@ -45,6 +53,7 @@ public class Cliente {
             System.out.println("Conexion del cliente terminada");
 
         } catch (IOException e) {
+            //Control errores
                 e.printStackTrace();
         }
     }
--- src/Fase_2/Servidor.java
+++ src/Fase_2/Servidor.java
@@ -9,31 +9,40 @@ import java.net.Socket;
 public class Servidor {
     public static void main(String[] args) {
 
+        //Declaración de variables
         ServerSocket servidor = null;
         Socket sc = null;
         DataInputStream in = null;
         DataOutputStream out = null;
 
+        //Puerto del servidor
         final int PUERTO = 5000;
 
         System.out.println("Servidor iniciado");
 
         try {
+            //crea socket
             servidor = new ServerSocket(PUERTO);
+            //Espera cliente
             sc = servidor.accept();
             System.out.println("Cliente conectado");
 
+            //Entrada, salida
             in = new DataInputStream(sc.getInputStream());
             out = new DataOutputStream(sc.getOutputStream());
 
+            //Variable control salida
             boolean salir = false;
 
             while (!salir) {
+                //Lee el mensaje del cliente
                 String mensaje = in.readUTF();
                 System.out.println("Cliente: " + mensaje);
 
+                //Si el mensaje es FIN, termina la conexión
                 if (mensaje.equalsIgnoreCase("FIN")) {
                     salir = true;
+                    //Mensaje de cierre
                     out.writeUTF("Conexión terminada");
                 } else {
                     out.writeUTF("Servidor: " + mensaje);
